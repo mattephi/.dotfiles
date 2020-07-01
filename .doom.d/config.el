244f09813255d0ae872d7524555b768f606ef717
m8dotpie <alentev.olm@ya.ru>
2020-07-01 20:48:21 +0300

    Added doom config

---
+++ .doom.d/config.el
@@ -0,0 +1,11 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Igor Alentev"
+      user-mail-address "alentev.olm@ya.ru")
+
+(setq doom-font (font-spec :family "JetBrains Mono" :size 12 :weight 'light)) ;; Doom main font
+(setq doom-theme 'doom-dracula) ;; Doom main theme
+
+(setq org-directory "~/org/") ;; Default org location
+
+(setq display-line-numbers-type 'relative) ;; Relative line numbers
